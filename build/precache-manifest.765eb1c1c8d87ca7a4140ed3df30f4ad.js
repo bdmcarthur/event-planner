39ec17eef90a3c069393811f9d11c468b42e70c8
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-12-28 23:17:32 -0500

    Created build folder

---
+++ build/precache-manifest.765eb1c1c8d87ca7a4140ed3df30f4ad.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bda859cd95df410b51b8a2594efe152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d2687f20452b9549ac",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "89aefd266b042acb005d",
+    "url": "/static/css/main.5c573f3c.chunk.css"
+  },
+  {
+    "revision": "c6d2687f20452b9549ac",
+    "url": "/static/js/2.5f502c4e.chunk.js"
+  },
+  {
+    "revision": "2a62585ff2bd5b248dddfab42ff4eac3",
+    "url": "/static/js/2.5f502c4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89aefd266b042acb005d",
+    "url": "/static/js/main.b875598e.chunk.js"
+  },
+  {
+    "revision": "f970ac2f437fed09d945",
+    "url": "/static/js/runtime-main.cf9ecc68.js"
+  },
+  {
+    "revision": "48eef0f48c13cd88bd5f1e1433c6e5b3",
+    "url": "/static/media/1.48eef0f4.jpg"
+  },
+  {
+    "revision": "6be42aee245ea5ba49ae34d6f92da3e9",
+    "url": "/static/media/2.6be42aee.jpg"
+  },
+  {
+    "revision": "0f513f0eb7db0c2bb2584156eb8388f3",
+    "url": "/static/media/3.0f513f0e.jpg"
+  },
+  {
+    "revision": "cb23195b72220005026e3dea1c16f7f1",
+    "url": "/static/media/4.cb23195b.jpg"
+  },
+  {
+    "revision": "d548a1eef06840f3c21cf92e79773be8",
+    "url": "/static/media/5.d548a1ee.jpg"
+  },
+  {
+    "revision": "8efecd774d8df91b57c2a4a13a0a40e7",
+    "url": "/static/media/6.8efecd77.jpg"
+  },
+  {
+    "revision": "a5072af32bceb2dc3cf9c2b3a11df61e",
+    "url": "/static/media/home.a5072af3.png"
+  }
+]);
